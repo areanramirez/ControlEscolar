311e77b6f6b5f80cb7ce82603ae3762a47336625
areanramirez <66035542+areanramirez@users.noreply.github.com>
2020-07-10 22:47:08 -0500

    Create MenuAlumno.h

---
+++ MenuAlumno.h
@@ -0,0 +1,18 @@
+/*
+ * MenuAlumno.h
+ *
+ *  Created on: Jul 2, 2020
+ *      Author: curso
+ */
+
+#ifndef MENUALUMNO_H_
+#define MENUALUMNO_H_
+
+class MenuAlumno {
+public:
+	MenuAlumno();
+	virtual ~MenuAlumno();
+	int mMAl();
+};
+
+#endif /* MENUALUMNO_H_ */
