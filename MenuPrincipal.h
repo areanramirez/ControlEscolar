9533b4acdbc743d9a35f9c25f239e288ac893580
areanramirez <66035542+areanramirez@users.noreply.github.com>
2020-07-10 23:05:16 -0500

    Create MenuPrincipal.h

---
+++ MenuPrincipal.h
@@ -0,0 +1,17 @@
+/*
+ * MenuPrincipal.h
+ *
+ *  Created on: Jun 30, 2020
+ *      Author: curso
+ */
+#ifndef MENUPRINCIPAL_H_
+#define MENUPRINCIPAL_H_
+
+class MenuPrincipal {
+public:
+	MenuPrincipal();
+	virtual ~MenuPrincipal();
+	int mMP();
+};
+
+#endif /* MENUPRINCIPAL_H_ */
